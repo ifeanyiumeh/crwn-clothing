ecf0448225bff57e5e55ba81be7c79388bee41b1
Ifeanyi Umeh <somto_umeh@yahoo.com>
2021-11-26 18:29:15 -0500

    added cart icon and cart dropdown componets to the application  and  also implemented cart reducer

--- src/components/header/testiondf.jsx
+++ src/components/header/testiondf.jsx
@@ -1,17 +1,14 @@
 import React from "react";
-
+import { Link } from "react-router-dom";
 import { connect } from "react-redux";
+
+import { auth } from "../../firebase/firebase.utils";
+import CartIcon from "../cart-icon/cart-icon.component";
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
-import { Link } from "react-router-dom";
-
 import "./header.styles.scss";
-
-import { auth } from "../../firebase/firebase.utils";
-
-import CartIcon from "../cart-icon/cart-icon.components";
-import CartDropdown from "../cart-dropdown/cart-dropdown.compnents";
 
 const Header = ({ currentUser, hidden }) => (
   <div className="header">
